5412bfe1ca380d4d47b325f635d2f073d0ead151
Ma Huihuang <007mhh@gmail.com>
2025-11-21 21:28:30 +0800

    solution: refactor go/airport-robot

---
+++ go/airport-robot/airport_robot.go
@@ -0,0 +1,36 @@
+package airportrobot
+
+import "fmt"
+
+// Write your code here.
+// This exercise does not have tests for each individual task.
+// Try to solve all the tasks first before running the tests.
+
+type Greeter interface {
+	LanguageName() string
+	Greet(string) string
+}
+
+func SayHello(visitor string, greeter Greeter) string {
+	return fmt.Sprintf("I can speak %s: %s", greeter.LanguageName(), greeter.Greet(visitor))
+}
+
+type Italian struct{}
+
+func (Italian) LanguageName() string {
+	return "Italian"
+}
+
+func (Italian) Greet(visitor string) string {
+	return fmt.Sprintf("Ciao %s!", visitor)
+}
+
+type Portuguese struct{}
+
+func (Portuguese) LanguageName() string {
+	return "Portuguese"
+}
+
+func (Portuguese) Greet(visitor string) string {
+	return fmt.Sprintf("Olá %s!", visitor)
+}
