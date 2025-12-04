e81f2869c619b62578d25e4230775473ac1557fa
Ma Huihuang <007mhh@gmail.com>
2025-12-04 10:49:44 +0800

    solution: go/roman-numerals

---
+++ go/roman-numerals/roman_numerals.go
@@ -0,0 +1,32 @@
+package romannumerals
+
+import (
+	"fmt"
+)
+
+type RomanNumeral struct {
+	Roman  rune
+	Arabic int
+}
+
+func ToRomanNumeral(input int) (string, error) {
+	if input < 1 || input > 3999 {
+		return "", fmt.Errorf("input must between 0 and 4000")
+	}
+
+	romanNumerals := map[int]string{
+		3000: "MMM", 2000: "MM", 1000: "M",
+		900: "CM", 800: "DCCC", 700: "DCC", 600: "DC", 500: "D", 400: "CD", 300: "CCC", 200: "CC", 100: "C",
+		90: "XC", 80: "LXXX", 70: "LXX", 60: "LX", 50: "L", 40: "XL", 30: "XXX", 20: "XX", 10: "X",
+		9: "IX", 8: "VIII", 7: "VII", 6: "VI", 5: "V", 4: "IV", 3: "III", 2: "II", 1: "I",
+	}
+	output := ""
+	for i := 1000; i > 0; i /= 10 {
+		div := input / i
+		if div > 0 {
+			output += romanNumerals[div*i]
+			input %= i
+		}
+	}
+	return output, nil
+}
